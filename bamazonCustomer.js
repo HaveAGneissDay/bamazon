879b2e7e160899c00fe580f936d9f6fe1acf2d8d
David Cheng <dkcheng10@gmail.com>
2018-02-07 15:35:24 -0800

    intial set up and psudo code

---
+++ bamazonCustomer.js
@@ -0,0 +1,28 @@
+// need inquirer
+// need mysql
+
+// connect to mysql database
+
+// create menu, via inquirer
+// products for sale (functions)
+// low inventory (functions)
+// add to inventory (functions)
+// add new product (functions)
+// switch statement, call the above functions
+
+
+// function currentProducts()
+// query database, select every item inside db
+// list ids, names, prices, and quantities
+
+// function lowInventory()
+// list all items with inventory count < 5
+
+// function addInventory()
+// prompt (inquirer) manager to add more
+// items to current product
+// select current item, update sql db
+
+// function newProduct()
+// add new product to db
+// inquirer to prompt item name, cost, department, etc match to your db
