3229dab59e0ac6b23554e3a584f2469fa4e1d0ee
David Cheng <dkcheng10@gmail.com>
2018-02-08 12:35:49 -0800

    inquirer prompt works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,47 @@
 // need inquirer
+var inquirer = require('inquirer');
 // need mysql
+var mysql = require('mysql');
 
 // connect to mysql database
+var connection = mysql.createConnection({
+    host: "bamazon",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazonDB"
+});
+
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId + "\n");
+// });
 
 // create menu, via inquirer
+inquirer.prompt([ {
+    type: "list",
+    name: "mainmenu",
+    message: "How may I help you today?",
+    choices: ["Products for Sale", "Add to Inventory", "Add New Product"]
+}
+]
+).then(
+    function () {
+    switch (choices) {
+        case choices === "Products for Sale":
+        console.log("Here are the products for sale");
+        break;
+        case choices === "Add to Inventory":
+        console.log("What would you like to add to the inventory?");
+        break;
+        default:
+        console.log("Have a nice day!");
+    }
+});
 // products for sale (functions)
 // low inventory (functions)
 // add to inventory (functions)
---
+++ schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  position INT NOT NULL,
+  item_ID INT NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100)NULL,
+  price DECIMAL(10,2) NULL,
+  quantitiy INT NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM products;
